ac0cda74c76f73e69934ca4159a1db42b37e130d
Sufiyan <sufiyanirfan235@gmail.com>
2024-02-29 09:02:00 +0530

    first

--- student_management/urls.py
+++ student_management/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from . import views
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",views.home,name='home'),
     path("std/",include("std_app.urls"))
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
